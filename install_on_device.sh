b5c73cc6412110b98f12769286e7c841738f6fd7
rbw@sla-mobile.co.za <richard@barnes-webb.com>
2016-03-31 08:24:46 +0100

    Programming the flux capacitor

---
+++ install_on_device.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+#  auth: rbw
+#  date: 20160331
+#  desc: 
+#
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+BASE_DIR=`cd "${0%/*}/." && pwd`
+
+APK="${BASE_DIR}/app/build/outputs/apk/app-debug.apk"
+
+[ ! -f "$APK" ] && $BASE_DIR/gradlew assembleDebug
+[ -z "$ANDROID_HOME" ] && echo "ERROR: ANDROID_HOME not set" && exit 13
+
+[ $(uname -o) = "Cygwin" ] && APK="$(cygpath -wa $APK)"
+
+$ANDROID_HOME/platform-tools/adb -d install -r $APK
+
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+echo "Done."
+#//EOF
